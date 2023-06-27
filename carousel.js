ffeea6737b24605494be66e6538f68ff862f6d81
nurealammiaji <nurealam.miaji@gmail.com>
2023-06-27 19:32:05 +0600

    images added

--- carousel.js
+++ carousel.js
@@ -1,3 +1,11 @@
-setInterval( () => {
-    console.log("Trying....")
-},1000);
+let images = [
+    './images/pic-1.jpg',
+    './images/pic-2.jpg',
+    './images/pic-3.jpg',
+    './images/pic-4.jpg',
+    './images/pic-5.jpg',
+    './images/pic-6.jpg',
+    './images/pic-7.jpg',
+    './images/pic-8.jpg',
+    './images/pic-9.jpg'
+];
